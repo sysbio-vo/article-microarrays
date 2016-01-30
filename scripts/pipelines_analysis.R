6b5e079f5270abe27ca5d0990b273587e279ef42
aln <alnfgit@gmail.com>
2016-01-30 22:36:46 +0200

    some refactoring

--- scripts/pipelines_analysis.R
+++ scripts/pipelines_analysis.R
@@ -1,86 +1,79 @@
-# Load HuGene and hgu133 BrainArray packages
-library(hugene10sthsentrezgprobe)
-library(hugene10sthsentrezgcdf)
-library(hugene10sthsentrezg.db)
-library(hgu133plus2hsentrezgprobe)
-library(hgu133plus2hsentrezgcdf)
-library(hgu133plus2hsentrezg.db)
-# Load Bioconductor annotation packages
-library(hgu133plus2.db)
-library(lumiHumanAll.db)
-# Other libs
-library(affy)
-
-### Initial info
-studies <- read.table("../pdata/studies.tsv", header = TRUE, sep = "\t")
-genes_common <- read.table("../expression_data/common_genes.txt", header = FALSE, sep = "\t")
-
-### RNA-seq data
-rnaseqExprs <- read.table("../expression_data/rnaseq_data_processed_sum.tsv", sep="\t", header=TRUE)
-
-### Brainarray
-pipe_type <- "brainarray"
-ind <- which(grepl("Affy", studies$platform))
-i = 2
-# Reading phenodata
-pdata <-read.table(paste("../pdata/pdata_", studies[ind[i],]$ID, ".txt", sep=""), header=TRUE, sep="\t")
-# Reading expression data
-eset<-read.table(paste("../expression_data/", studies[ind[i],]$ID, "_", pipe_type, ".txt", sep=""), header=TRUE)
-# List of probesets IDs
-probesetsID<-rownames(eset)
-# List of corresponding gene IDs
-detach("package:MASS", unload=TRUE)
-probesetsID_EntrezID<-select(get(paste(studies[ind[i],]$platformAbbr, "hsentrezg.db", sep="")), probesetsID, "ENTREZID")
-# Replace probesetsIDs with gene IDs in expression data matrix
-eset <- eset[which(probesetsID_EntrezID$ENTREZID!="NA"),]
-rownames(eset) <- probesetsID_EntrezID$ENTREZID[which(probesetsID_EntrezID$ENTREZID!="NA")]
-# Select only common genes for all the platforms and pipelines
-eset <- eset[rownames(eset) %in% genes_common[,1],]
-# Select TNBC samples only
-pdata<-pdata[which(pdata$CancerType=="TNBC"),]
-eset <- eset[colnames(eset) %in% pdata$SampleAccessionNumber]
-# Select common genes with rna-seq
-eset <- eset[rownames(eset) %in% rnaseqExprs$entrezID,]
-eset <- data.frame(val=rowMeans(eset), row.names=rownames(eset))
-# Organize rna-seq dataset
-rownames(rnaseqExprs) <- rnaseqExprs$entrezID
-rnaseqExprs <- rnaseqExprs[-1]
-rnaseqExprs$val <- rnaseqExprs[order(match(rownames(rnaseqExprs),rownames(eset))),]
-rownames(rnaseqExprs) <- rownames(eset)
-# Scatter plot
 library("MASS")
 library(RColorBrewer)
 library(lmtest)
 library(stats)
+
+
+### Initial info
+studies <- read.table("../pdata/studies.tsv", header = TRUE, sep = "\t")
+affy <- which(grepl("Affy", studies$platform))
+pipe_type <- "brainarray"
+
+# Create classes for storing pipelines results
+setClass("dataset",
+         representation(ID="character", exprs="data.frame"))
+setClass("pipeline",
+         representation(name="character", dataset="dataset"))
+pipelines <- c()
+
+for (i in affy) {
+  exprs <- read.table(paste("../expression_data/", studies[affy[i],]$ID, "_exprs_", pipe_type, ".tsv", sep=""), header = TRUE, sep = "\t")
+  dataset <- new("dataset", ID=as.character(studies[affy[i],]$ID), exprs=exprs)
+  pipelines <- c(pipelines, new("pipeline", name=pipe_type, dataset=dataset))
+}
+
+ind = 1
+eset <- pipelines[[ind]]@dataset@exprs$eset
+rnaseq <- pipelines[[ind]]@dataset@exprs$rnaseq
+
+# Fit linear and polynomial functions:
+lin<-lm(rnaseq~eset)
+quad<-lm(rnaseq~eset+I(eset^2)+I(eset^3))
+
+# Scatter plot
 k <- 11
 my.cols <- rev(brewer.pal(k, "RdYlBu"))
-z <- kde2d(eset$val, rnaseqExprs$val, n=50)
-plot(eset$val,rnaseqExprs$val, xlab="Probeset intensity", ylab="log2 FPKM", main=paste(studies$ID[ind[i]],pipe_type),pch="*", cex=.4)
+z <- kde2d(eset, rnaseq, n=50)
+plot(eset, rnaseq, xlab="log2 probeset intensity", ylab="log2 FPKM", main=paste(studies$ID[affy[ind]],pipe_type),pch="*", cex=.4)
 contour(z, drawlabels=FALSE, nlevels=k, col=my.cols, add=TRUE)
-abline(h=median(rnaseqExprs$val), v=median(eset$val), lwd=1)
-# Fit linear model:
-lm_exprs<-lm(rnaseqExprs$val~eset$val+I(eset$val^2)+I(eset$val^3))
-sumry_lm<-summary(lm_exprs)
-value_add<-sumry_lm$r.squared
-index <- order(eset$val)
-lines(eset$val[index], predict(lm_exprs)[index], col="red")
-n=min(eset$val)+1
-text(n,8,paste("R=",round(value_add,digits = 3),sep="",collapse = ""),col="red",font=2)
+# Draw medians
+abline(h=median(rnaseq), v=median(eset), lwd=1)
+# Draw regression lines
+r_lin <- summary(lin)$r.squared
+r_quad <- summary(quad)$r.squared
+index <- order(eset)
+lines(eset[index], predict(lin)[index], col="red", lwd=2)
+lines(eset[index], predict(quad)[index], col="blue", lwd=2)
+n=min(eset)+1
+m=max(eset)-1
+text(n, 8, paste("R=",round(r_lin, digits = 3),sep="",collapse = ""),col="red",font=2)
+text(m, 8, paste("R=",round(r_quad, digits = 3),sep="",collapse = ""),col="blue",font=2)
+
 # Fit non-linear model
 #loess_fit <- loess(rnaseqExprs$val~eset$val)
 #lines(eset$val[index], predict(loess_fit)[index], col="blue")
 
-dev.copy(pdf,paste("../plots/", studies[ind[i],]$ID, "_scatter_brain_sum_quadr.pdf", sep=""))
+dev.copy(pdf, paste("../plots/", pipe_type, "/", studies[affy[ind],]$ID, "_scatterplot_", pipe_type,".pdf", sep=""))
 dev.off()
 
+# ggplot2
+library(ggplot2)
+commonTheme = list(labs(x="log2 probeset intensity",
+                        y="log2 FPKM"),
+                   theme_bw()+theme(legend.position="none"))
+ggplot(data=df,aes(x,y)) + 
+  geom_point(alpha=0.3) +
+  stat_density2d(aes(fill=..level..,alpha=..level..),geom='polygon',colour='black', alpha=0.5) + 
+  scale_fill_continuous(low="#2FFF71", high="#FF4C48") +
+  geom_smooth(method=lm, linetype=1, colour="black", se=F, size=2) + 
+  geom_smooth(method=lm, formula=y~x+I(x^2)+I(x^3), linetype=1, colour="blue", se=F, size=2) + 
+  commonTheme
+
+
 # Test for linearity
-model1<-lm(rnaseqExprs$val~eset$val)
-model2<-lm(rnaseqExprs$val~eset$val+I(eset$val^2)+I(eset$val^3))
-pvalue <- anova(model1, model2)$"Pr(>F)"
-a <- anova(model1, model2)
-pvalue
-stats:::plot.lm(model1)
-stats:::plot.lm(model2)
+pvalue <- anova(lin, quad)$"Pr(>F)"
+stats:::plot.lm(lin, ask = FALSE, which=3)
+stats:::plot.lm(quad, ask = FALSE, which=3)
 
 # Another test for linearity
 resettest(rnaseqExprs$val~eset$val, power=3, type="regressor")
