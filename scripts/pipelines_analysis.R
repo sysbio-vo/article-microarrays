a5e7e968d8c8ed941bdaf076a3780d67a0b05324
aln <alnfgit@gmail.com>
2016-01-31 23:14:38 +0200

    Save several plots in one pdf file. Regular ggplot solution doesn't work as it doesn't scale text/ticks etc with the plot. cowplot package is used (align arg in plot_grid function)

--- scripts/pipelines_analysis.R
+++ scripts/pipelines_analysis.R
@@ -1,8 +1,8 @@
-library("MASS")
-library(RColorBrewer)
 library(lmtest)
 library(stats)
-
+library(cowplot)
+library(plyr)
+library(ggplot2)
 
 ### Initial info
 studies <- read.table("../pdata/studies.tsv", header = TRUE, sep = "\t")
@@ -22,7 +22,8 @@ for (i in affy) {
   pipelines <- c(pipelines, new("pipeline", name=pipe_type, dataset=dataset))
 }
 
-ind = 3
+plot_list = list()
+for (ind in 1:length(affy)) {
 eset <- pipelines[[ind]]@dataset@exprs$eset
 rnaseq <- pipelines[[ind]]@dataset@exprs$rnaseq
 
@@ -36,17 +37,17 @@ r_quad <- summary(quad)$r.squared
 #lines(eset$val[index], predict(loess_fit)[index], col="blue")
 
 # Scatter plot
-library(ggplot2)
 df <- data.frame(x=eset, y=rnaseq)
 commonTheme = list(labs(x="log2 probeset intensity",
                         y="log2 FPKM", size=16),
                    theme_bw()+
                    theme(legend.position="none",
+                         plot.margin=unit(c(5,5,5,5),"mm"),
                          axis.text = element_text(size=18),
                          axis.title = element_text(size=16),
                          plot.title = element_text(face="bold", size=22)))
 
-ggplot(data=df,aes(x,y)) + 
+new_plot <- ggplot(data=df,aes(x,y)) + 
   geom_point(alpha=0.3) +
   stat_density2d(aes(fill=..level..,alpha=..level..),geom='polygon',colour='black', alpha=0.5) + 
   scale_fill_continuous(low="#2FFF71", high="#FF4C48") +
@@ -54,15 +55,25 @@ ggplot(data=df,aes(x,y)) +
   geom_smooth(method=lm, formula=y~x+I(x^2)+I(x^3), linetype=1, colour="blue", se=F, size=2) + 
   geom_vline(xintercept = median(df$x), linetype=2) + 
   geom_hline(yintercept = median(df$y), linetype=2) + 
-  annotate("text", x=min(df$x)+1, y=max(df$y), label=paste("R=",round(r_lin, digits = 3)), color="black", size=6) +
-  annotate("text", x=min(df$x)+1, y=max(df$y)-1, label=paste("R=",round(r_quad, digits = 3)), color="blue", size=6) +
-  scale_x_continuous(breaks = trunc(seq(trunc(min(df$x)), max(df$x), by = 2), 1)) +
-  scale_y_continuous(breaks = trunc(seq(trunc(min(df$y)), max(df$y), by = 2), 1)) +  
+  annotate("text", x=min(df$x)+1.5, y=max(df$y), label=paste("R=",round(r_lin, digits = 3)), color="black", size=6) +
+  annotate("text", x=min(df$x)+1.5, y=max(df$y)-1, label=paste("R=",round(r_quad, digits = 3)), color="blue", size=6) +
+  scale_x_continuous(breaks = round(seq(round_any(min(df$x),2), max(df$x), by = 2), 1)) +
+  scale_y_continuous(breaks = round(seq(round_any(min(df$y),2), max(df$y), by = 2), 1)) +  
   ggtitle(paste(studies$ID[affy[ind]],pipe_type)) +
+  coord_fixed() +
   commonTheme
+  plot_list <- c(plot_list, list(new_plot))
+}
 
-dev.copy(pdf, paste("../plots/", pipe_type, "/", studies[affy[ind],]$ID, "_scatterplot_", pipe_type,".pdf", sep=""))
-dev.off()
+pl <- plot_grid(plotlist=plot_list, ncol=3, align="hv")
+save_plot(paste("../plots/", pipe_type, "/all_scatterplot_", pipe_type,".pdf", sep=""), pl,
+          ncol = 3,
+          nrow = 1,
+          base_height=6,
+          base_aspect_ratio=0.7)
+
+#dev.copy(pdf, paste("../plots/", pipe_type, "/", studies[affy[ind],]$ID, "_scatterplot_", pipe_type,".pdf", sep=""))
+#dev.off()
 
 # Test for linearity
 pvalue <- anova(lin, quad)$"Pr(>F)"
