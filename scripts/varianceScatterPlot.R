fb4a36d57835b7ba44db22774e846168a00157fb
aln <alnfgit@gmail.com>
2016-03-03 21:54:46 +0200

    Probesets per gene variance study

---
+++ scripts/varianceScatterPlot.R
@@ -0,0 +1,44 @@
+scatterPlot <- function(df, title, ranges) {
+  # TODO: replace the constants with passing arguments
+  minx=ranges[1]
+  maxx=ranges[2]
+  miny=ranges[3]
+  maxy=ranges[4]
+  
+  commonTheme = list(labs(x="log2 FPKM",
+                          y="SD of probesets groups", size=16),
+                     theme_bw()+
+                       theme(legend.position="none",
+                             plot.margin=unit(c(5,5,5,5),"mm"),
+                             axis.text = element_text(size=18),
+                             axis.title = element_text(size=16),
+                             plot.title = element_text(face="bold", size=18)))
+
+  if (simple) {
+    p <- ggplot(data=df,aes(x=rnaseq,y=eset)) + 
+      geom_point(alpha=0.3) +
+      stat_density2d(aes(fill=..level..,alpha=..level..),geom='polygon',colour='black', alpha=0.5) + 
+      scale_fill_continuous(low="#3E9EFF", high="#B23830") +
+      geom_vline(xintercept = median(df$rnaseq), linetype=2) + 
+      geom_hline(yintercept = median(df$eset), linetype=2) + 
+      scale_x_continuous(breaks = seq(minx, maxx, by = 2), limits=c(minx, maxx)) +
+      scale_y_continuous(breaks = round(seq(round_any(min(df$rnaseq),2), max(df$rnaseq), by = 2), 1), limits=c(miny, maxy)) +  
+      ggtitle(title) +
+#      coord_fixed() +
+      commonTheme
+  } else {
+    p <- ggplot(data=df,aes(x=rnaseq, y=Mean, ymin=Upper, ymax=Lower)) + 
+      geom_pointrange(size=1, alpha=0.3) +
+      stat_density2d(aes(fill=..level..,alpha=..level..),geom='polygon',colour='black', alpha=0.5) + 
+      scale_fill_continuous(low="#3E9EFF", high="#B23830") +
+      geom_vline(xintercept = median(df$rnaseq), linetype=2) + 
+      geom_hline(yintercept = median(df$Mean), linetype=2) + 
+      scale_x_continuous(breaks = seq(minx, maxx, by = 2), limits=c(minx, maxx)) +
+      scale_y_continuous(breaks = round(seq(round_any(min(df$rnaseq),2), max(df$rnaseq), by = 2), 1), limits=c(miny, maxy)) +  
+      ggtitle(title) +
+      coord_fixed() +
+      commonTheme
+  }
+  
+  return(p)
+}
---
+++ scripts/variation_analysis.R
@@ -0,0 +1,125 @@
+# Load HuGene and hgu133 BrainArray packages
+library(hgu133plus2.db)
+library(hugene10sttranscriptcluster.db)
+library(illuminaHumanv4.db)
+library(reshape2)
+library(ggplot2)
+library(cowplot)
+library(plyr)
+library(stats)
+library(Hmisc)
+
+### Initial info
+studies <- read.table("../general/studies.tsv", header = TRUE, sep = "\t")
+freq_list <- data.frame()
+varplot_list <- c()
+simple=FALSE
+
+# Generate aggregated microarray-rnaseq expression files for plots feeding
+for (i in 1:length(studies$ID)) {
+  # Reading expression data
+  if (grepl("Affy", studies[i,]$platform)) {
+    platform <- "affymetrix"
+  } else {
+    platform <- "illumina"
+  }
+  eset<-read.table(paste("../preprocessed/", studies[i,]$ID, "_preprocessed_", platform, ".tsv", sep=""), header=TRUE)
+
+  # Reading phenodata. Select TNBC only
+  pdata <-read.table(paste("../pdata/pdata_", studies[i,]$ID, ".tsv", sep=""), header=TRUE, sep="\t")
+  pdata<-pdata[which(pdata$CancerType=="TNBC"),]
+  pdata<-pdata[which(is.na(pdata$Outliers)),]
+  eset <- eset[,colnames(eset) %in% pdata$SampleAccessionNumber]
+  
+  probesetsID<-rownames(eset)
+  if (studies[i,]$platformAbbr=="hugene10st") {
+    probesetsID_EntrezID<-select(get(paste(studies[i,]$platformAbbr, "transcriptcluster.db", sep="")), probesetsID, "ENTREZID")
+  } else {
+    probesetsID_EntrezID<-select(get(paste(studies[i,]$platformAbbr, ".db", sep="")), probesetsID, "ENTREZID")
+  }
+  # Remove NA
+  probesetsID_EntrezID <- probesetsID_EntrezID[which(probesetsID_EntrezID$ENTREZID!="NA"),]
+  # Duplicate probes
+  n_occur <- data.frame(table(probesetsID_EntrezID$PROBEID))
+  uniques <- n_occur[n_occur$Freq == 1,]$Var1
+  probesetsID_EntrezID <- probesetsID_EntrezID[which(probesetsID_EntrezID$PROBEID %in% uniques),]
+  # Filter eset based on left probesets
+  eset <- eset[which(rownames(eset) %in% probesetsID_EntrezID$PROBEID),]
+
+  # Analyze probesets mapped to one gene
+  n_occur <- data.frame(table(probesetsID_EntrezID$ENTREZID))
+  dupls <- n_occur[n_occur$Freq > 1,]
+#   freq <- data.frame(table(dupls$Freq))
+#   colnames(freq)[1] <- "ProbesetsNum"
+#   freq$Study <- rep(studies[i,]$ID, nrow(freq))
+#   freq_list <- structure(rbind(freq_list,freq), .Names = names(freq))
+
+  probesetsID_EntrezID <- probesetsID_EntrezID[which(probesetsID_EntrezID$ENTREZID %in% dupls$Var1),]
+  eset <- eset[which(rownames(eset) %in% probesetsID_EntrezID$PROBEID),]
+  eset$ENTREZID <- probesetsID_EntrezID$ENTREZID
+  if (simple) {
+    eset <- data.frame(val=rowMeans(eset), row.names=rownames(eset))
+  } else {
+    eset <- melt(eset)
+    eset <- eset[,-2] 
+  }
+
+  rnaseq <- read.table("../rnaseq/rnaseq_data_processed_sum_long.tsv", sep="\t", header=TRUE)
+  rnaseq <- rnaseq[rnaseq$entrezID %in% eset$ENTREZID,]
+  eset <- eset[eset$ENTREZID %in% rnaseq$entrezID,]
+
+  uniques <- unique(eset$ENTREZID)
+  variance <- c()
+  for (j in 1:length(uniques)) {
+    if (simple) {
+      variance <- c(variance, sd(eset[eset$ENTREZID %in% uniques[j],]$val))
+    } else {
+      variance <- c(variance, smean.cl.boot(eset[eset$ENTREZID %in% uniques[j],]$value))
+    }
+  }
+  if (!simple) {
+    var <- data.frame(Mean=double(), Lower=double(), Upper=double(), stringsAsFactors=FALSE)
+    j=1
+    while (j<=length(uniques)*3) {
+      var[nrow(var)+1,] <- variance[j:(j+2)]
+      j = j+3
+    }
+    #eset <- data.frame(row.names=uniques, ENTREZID=uniques, Mean=var$Mean, Lower=var$Lower, Upper=var$Upper)
+    eset <- data.frame(row.names=uniques, ENTREZID=uniques, variance=(var$Upper - var$Lower))
+  } else {
+    eset <- data.frame(row.names=uniques, ENTREZID=uniques, variance=variance)
+  }
+
+  exprs <- merge(eset, rnaseq, by.x = "ENTREZID", by.y="entrezID")
+  if (simple) {
+    colnames(exprs) <- c("ENREZID", "eset", "rnaseq")
+  } else {
+    colnames(exprs) <- c("ENREZID", "Mean", "Lower", "Upper", "rnaseq")
+  }
+
+  source("varianceScatterPlot.R")
+  new_plot <- scatterPlot(exprs, paste(studies[i,]$ID, sep=""), c(-7, 11, 0, 2))
+  varplot_list <- c(varplot_list, list(new_plot))  
+}
+
+pl <- plot_grid(plotlist=varplot_list, ncol=2, nrow=2, align="v")
+save_plot("../plots/onetomany_bootstrap_variance_scatterplot.pdf", pl,
+          base_height=4, base_aspect_ratio=1.5, nrow=2, ncol=2)
+
+save_plot("../plots/onetomany_bootstrap_variance_scatterplot.pdf", new_plot,
+          base_height=10)
+
+# pl <- ggplot(data=freq_list, aes(x=ProbesetsNum, y=Freq, fill=ProbesetsNum)) +
+#   geom_bar(stat="identity") +
+#   facet_grid(~Study) + 
+#   geom_text(aes(y=Freq, ymax=Freq, label=Freq),
+#             position = position_dodge(width=0.9), vjust=-.5, color="black") + 
+#   theme_gray() +
+#   theme(legend.position = "none") +
+#   labs(x = "Number of probesets mapped to one gene",
+#        y = "Overall frequency")
+# 
+# save_plot("../plots/probesets_per_gene_frequency.pdf", pl,
+#           base_height=8, base_aspect_ratio=2.1)
+
+
